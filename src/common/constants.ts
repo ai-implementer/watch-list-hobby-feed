100854bb35af62ff39f54722cbe951084630a645
ryuone <ryuone@gmail.com>
2026-02-01 00:17:49 +0900

    chore: update site configuration and URLs for new repository

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,20 +1,20 @@
-const siteUrlStem = 'https://yamadashy.github.io/tech-blog-rss-feed';
+const siteUrlStem = 'https://ai-implementer.github.io/watch-list-hobby-feed';
 const siteUrl = `${siteUrlStem}/`;
 
 export default {
   // サイト設定
   siteUrl: `${siteUrl}`,
   siteUrlStem: siteUrlStem,
-  siteTitle: '企業テックブログRSS',
+  siteTitle: 'ブログRSS',
   siteDescription:
     '企業のテックブログの更新をまとめたRSSフィードを配信しています。記事を読んでその企業の技術・カルチャーを知れることや、質の高い技術情報を得られることを目的としています。',
 
   // フィード設定
-  feedTitle: '企業テックブログRSS',
+  feedTitle: 'ブログRSS',
   feedDescription: '企業のテックブログの更新をまとめたRSSフィード',
   feedLanguage: 'ja',
-  feedCopyright: 'yamadashy/tech-blog-rss-feed',
-  feedGenerator: 'yamadashy/tech-blog-rss-feed',
+  feedCopyright: 'ai-implementer/watch-list-hobby-feed',
+  feedGenerator: 'ai-implementer/watch-list-hobby-feed',
   feedUrls: {
     atom: `${siteUrl}feeds/atom.xml`,
     rss: `${siteUrl}feeds/rss.xml`,
@@ -22,21 +22,21 @@ export default {
   },
 
   // リンク
-  author: 'yamadashy',
-  gitHubUserUrl: 'https://github.com/yamadashy/',
-  gitHubRepositoryUrl: 'https://github.com/yamadashy/tech-blog-rss-feed/',
-  xUserUrl: 'https://x.com/yamadashy',
+  author: 'ai-implementer',
+  gitHubUserUrl: 'https://github.com/ai-implementer/',
+  gitHubRepositoryUrl: 'https://github.com/ai-implementer/watch-list-hobby-feed/',
+  xUserUrl: 'https://x.com/',
 
   // Google Analytics系。フォークして使う際は値を空にするか書き換えてください
-  googleSiteVerification: 'GPLvXv8kYtLMW912ZS54DKFEZL6ruOrjOFLdHVTo37o',
-  globalSiteTagKey: 'G-CNNNTL0NB3',
+  googleSiteVerification: '',
+  globalSiteTagKey: 'G-CL0G5METXQ',
 
   // フィードの取得などに使う UserAgent
-  requestUserAgent: 'facebookexternalhit/1.1; yamadashy/tech-blog-rss-feed',
+  requestUserAgent: 'facebookexternalhit/1.1; ai-implementer/watch-list-hobby-feed',
 
   // サイトの追加方法のリンク
   howToAddSiteLink:
-    'https://github.com/yamadashy/tech-blog-rss-feed#%E3%82%B5%E3%82%A4%E3%83%88%E3%81%AE%E8%BF%BD%E5%8A%A0%E6%96%B9%E6%B3%95',
+    'https://github.com/ai-implementer/watch-list-hobby-feed#%E3%82%B5%E3%82%A4%E3%83%88%E3%81%AE%E8%BF%BD%E5%8A%A0%E6%96%B9%E6%B3%95',
 
   // 処理の設定
   feedFetchConcurrency: 50, // フィードを取得する並列数
